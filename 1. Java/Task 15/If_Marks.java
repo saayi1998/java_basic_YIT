d221227614fb0951d3020b44aa1cd96b9b7af382
saayi1998 <ananthasaayisaayi@gmail.com>
2026-06-25 09:25:46 +0530

    java basic

---
+++ 1. Java/Task 15/If_Marks.java
@@ -0,0 +1,13 @@
+class If_Marks{
+	public static void main (String args[]){
+		int student_marks = 55;
+		
+		if (student_marks >=50){
+		System.out.println("PASS");
+		}
+		
+		else {
+		System.out.println("FAIL");
+		}
+	}
+}
