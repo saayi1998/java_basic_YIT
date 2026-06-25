104a910efd115ec8ea43602c37ebe5ad008511e8
saayi1998 <ananthasaayisaayi@gmail.com>
2026-06-25 14:41:51 +0530

    44444

---
+++ 1. Java/Task 02/PrintDemo.java
@@ -0,0 +1,18 @@
+class PrintDemo {
+	public static void main(String args[]){
+		System.out.println("My Name is Seelan");
+		System.out.println("My Father name is Yogan");
+		System.out.println("My age is 24");
+		
+		System.out.println();
+		System.out.print("My name is Seelan");
+		System.out.print("My Father name is Yogan");
+		System.out.print("My age is 24");
+		
+		System.out.println();
+		System.out.print("My name is Seelan\n");
+		System.out.print("My Father name is Yogan\n");
+		System.out.print("My age is 24\n");
+		
+	}
+}
