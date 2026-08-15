f4507f47ab7524f2e288cb616affe2f5a3f7fb67
saayi1998 <ananthasaayisaayi@gmail.com>
2026-08-16 00:02:33 +0530

    +

--- 1. Java/Task 60 - StarPattern6/StarPattern6.java
+++ 1. Java/Task 60 - StarPattern6/StarPattern6.java
@@ -1,11 +1,11 @@
 class StarPattern6{
 	public static void main(String args[]){
 		int n=5;
-		for(int rows=1;rows<=n;rows++){ 
+		for(int rows=1;rows<n;rows++){ 
 			for(int cols1=rows+1;cols1<=n;cols1++){
 				System.out.print(" ");
 			}	
-			for(int cols2=1;cols2<=2*rows-1;cols2++){
+			for(int cols2=1;cols2<=(2*rows)-1;cols2++){
 				System.out.print("*");
 			}
 		System.out.println();
